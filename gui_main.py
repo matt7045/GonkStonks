8f53c7a96652264acf39b69aec753d9dc440c136
NitrousHunter <77123737+NitrousHunter@users.noreply.github.com>
2021-01-28 18:57:58 -0500

    Add files via upload
    uploading main gui canvas and background image

---
+++ gui_main.py
@@ -0,0 +1,42 @@
+#! python3
+#boxes
+
+
+#import tkinter as tk
+from tkinter import *
+root = Tk()
+
+#display
+canvas_main = Canvas(root, width=400, height=300)
+canvas_main.pack()
+
+#background image
+stonks_pic = PhotoImage(file="stonks.gif")
+
+#place image
+canvas_main.create_image(50, 10, image=stonks_pic, anchor=NW)
+
+#create quantity input box
+quantity_entry = Entry(root)
+canvas_main.create_window(200, 100, window=quantity_entry)
+quantity = quantity_entry.get()
+
+#quantity label
+q_label = Label(root, text = 'quantity')
+canvas_main.create_window(150, 100, window=q_label)
+
+#create price input box
+price_entry = Entry(root)
+canvas_main.create_window(200, 140, window=price_entry)
+price = price_entry.get()
+
+#price label
+p_label = Label(root, text = 'price')
+canvas_main.create_window(150, 140, window=p_label)
+
+#buttonCliked button
+button_clicked = Button(text = 'buy stonks')# , command = buttonClicked)
+canvas_main.create_window(200, 180, window=button_clicked)
+#buttonClicked()
+
+root.mainloop()
