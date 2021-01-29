5e02f4444e5b5557102cb0cdeab56ba58bd3d907
Matthew Abazis <mattu1582@gmail.com>
2021-01-29 00:50:18 -0500

    Installs python quietly now

--- shitty_ass_code/init_windows.bat
+++ shitty_ass_code/init_windows.bat
@@ -5,10 +5,10 @@ IF NOT "%version:~0,8%"=="Python 3" (
     echo Installing Python 3
     IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     bitsadmin /transfer myDownloadJob /download /priority FOREGROUND https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe %CD%/py_installer39.exe
-    py_installer39.exe
+    py_installer39.exe /quiet PrependPath=1
     ) ELSE (
     bitsadmin /transfer myDownloadJob /download /priority FOREGROUND https://www.python.org/ftp/python/3.9.1/python-3.9.1.exe %CD%/py_installer39.exe
-    py_installer39.exe
+    py_installer39.exe /quiet PrependPath=1
     )
     del py_installer39.exe
 ) ELSE (
