0d2ee2d9d109d3df1ff922db301882dd45dc681f
Matthew Abazis <mattu1582@gmail.com>
2021-01-28 23:36:42 -0500

    Updated install to install faster, and included images in the README

--- shitty_ass_code/init_windows.bat
+++ shitty_ass_code/init_windows.bat
@@ -4,10 +4,10 @@ set /p version=<version.txt
 IF NOT "%version:~0,8%"=="Python 3" (
     echo Installing Python 3
     IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    bitsadmin /transfer myDownloadJob /download /priority normal https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe %CD%/py_installer39.exe
+    bitsadmin /transfer myDownloadJob /download /priority FOREGROUND https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe %CD%/py_installer39.exe
     py_installer39.exe
     ) ELSE (
-    bitsadmin /transfer myDownloadJob /download /priority normal https://www.python.org/ftp/python/3.9.1/python-3.9.1.exe %CD%/py_installer39.exe
+    bitsadmin /transfer myDownloadJob /download /priority FOREGROUND https://www.python.org/ftp/python/3.9.1/python-3.9.1.exe %CD%/py_installer39.exe
     py_installer39.exe
     )
     del py_installer39.exe
