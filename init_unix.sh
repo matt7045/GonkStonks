2a2233ce3d988796f6b97b5e986dec8a927a0de6
EelEyes <78172317+EelEyes@users.noreply.github.com>
2021-01-28 20:27:38 -0500

    Update init_unix.sh
    added pip install for alpaca_trade_api.

--- init_unix.sh
+++ init_unix.sh
@@ -8,6 +8,8 @@ else
   curl https://www.python.org/ftp/python/3.9.1/python-3.9.1-macosx10.9.pkg --output python.pkg
   open python.pkg
 fi
+pip install alpaca_trade_api
+
 
 echo 'Starting Gonk Stonks! Good Luck!'
 python main.py
